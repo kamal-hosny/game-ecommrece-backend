0010a15272a7e4e3e3a55fae9419c7342be41bfd
kamal <kamal@gmail.com>
2024-08-06 02:40:13 +0300

    kk

--- config/plugins.js
+++ config/plugins.js
@@ -1,19 +1,8 @@
 module.exports = ({ env }) => ({
-  // ...
   upload: {
-    config: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-      },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
-      },
+    provider: 'local',
+    providerOptions: {
+      sizeLimit: 1000000, // حجم الملفات المسموح بها
     },
   },
-  // ...
 });
