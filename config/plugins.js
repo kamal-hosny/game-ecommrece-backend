dee8b83bbf2d9463855b429c465422bd94536616
kamal <kamal@gmail.com>
2024-08-06 16:46:20 +0300

    fssf

--- config/plugins.js
+++ config/plugins.js
@@ -1,8 +1,1 @@
-module.exports = ({ env }) => ({
-  upload: {
-    provider: 'local',
-    providerOptions: {
-      sizeLimit: 1000000, // حجم الملفات المسموح بها
-    },
-  },
-});
+module.exports = () => ({});
