73210bb964a2493b9e994071373874aad6b19adc
kamal <kamal@gmail.com>
2024-08-07 23:58:55 +0300

    ddd

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,10 +1,6 @@
 module.exports = [
+  'strapi::logger',
   'strapi::errors',
-  'strapi::cors',
-  'strapi::query',
-  'strapi::body',
-  'strapi::public',
-  'strapi::favicon',
   {
     name: 'strapi::security',
     config: {
@@ -12,11 +8,24 @@ module.exports = [
         useDefaults: true,
         directives: {
           'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinary.com',
+          ],
           upgradeInsecureRequests: null,
         },
       },
     },
   },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
 ];
